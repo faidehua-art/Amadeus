3f8238d96b28d45edf00312eec221d96e96f1c77
faidehua-art <faidehua@gmail.com>
2025-11-02 21:35:57 +0800

    Update 3_main.c

--- 3_main.c
+++ 3_main.c
@@ -3,22 +3,51 @@
 //兰雄杰
 #include <stdio.h>
 
+void swap(int *a, int *b);
+void bubbleSort(int *a, int n);
+
 int main(void)
 {
-    int i,j;
-    char k;
+    int n = 10, a[n], *b = a;
 
-    scanf("%d %d %c",&i,&j,&k);
+    while(b < &a[n]) {
+        scanf("%d", b++);
+    }
 
-    if(k=='+') {
-        printf("%d",i+j);
-    }else if(k=='-') {
-        printf("%d",i-j);
-    }else if(k=='*') {
-        printf("%d",i*j);
-    }else if(k=='/') {
-        printf("%d",i/j);
+
+    bubbleSort(a, n);
+
+    b = a;
+    while(b < &a[n]) {
+        if(b > a) {
+            printf(" ");
+        }
+        printf("%d", *b++);
     }
 
     return 0;
+}
+
+void swap(int *a, int *b)
+{
+    int stemp;
+    stemp = *a;
+    *a = *b;
+    *b = stemp;
+}
+
+void bubbleSort(int *a, int n)
+{
+    int *c, swapped = 1;
+    while(swapped == 1) {
+        swapped = 0;
+        c = a;
+        while(c < &a[n - 1]) {
+            if(*c > *(c + 1)) {
+                swap(c, c + 1);
+                swapped = 1;
+            }
+            c++;
+        }
+    }
 }
